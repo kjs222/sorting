ef598da4a33bbcf07c1df19dc7cfa201df1780ec
Kerry <kerry.sheldon@gmail.com>
2016-03-31 16:59:38 -0600

    added test for sorting letters

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,7 +1,6 @@
 class BubbleSort
 
   def sort(unsorted, prev=0, cur=1, length=unsorted.length)
-    length = unsorted.length
     while length > 1
       while cur < length
         prev_val = unsorted[prev]
@@ -20,24 +19,5 @@ class BubbleSort
     end
     unsorted
   end
-
-##works for 1 bubble
-  # def sort(unsorted, prev=0, cur=1, length=unsorted.length)
-  #     while cur < length
-  #       prev_val = unsorted[prev]
-  #       cur_val = unsorted[cur]
-  #       if prev_val > cur_val
-  #         unsorted[prev] = cur_val
-  #         unsorted[cur] = prev_val
-  #       end
-  #       prev += 1
-  #       cur += 1
-  #       unsorted
-  #   end
-  #   unsorted
-  # end
-
-
-
 
 end
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -10,21 +10,17 @@ class BubbleSortTest < MiniTest::Test
     array1 = [2, 1]
     assert_equal [1, 2], bubble.sort(array1)
 
-
-
     array2 = [1, 2]
     assert_equal [1, 2], bubble.sort(array2)
 
     array3 = [2, 2]
     assert_equal [2, 2], bubble.sort(array3)
-
-
   end
 
   def test_completes_1_bubble
     bubble = BubbleSort.new
     array1 = [4, 3, 2, 1]
-    #assert_equal [3, 2, 1, 4], bubble.sort(array1)
+
     assert_equal 4, bubble.sort(array1)[-1]
   end
 
@@ -38,6 +34,13 @@ class BubbleSortTest < MiniTest::Test
 
     array3 = [0, -1, -10, 5]
     assert_equal [-10, -1, 0, 5], bubble.sort(array3)
+
+  end
+
+  def test_sorts_letters
+    bubble = BubbleSort.new
+    array1 = ["d", "c", "z", "r"]
+    assert_equal ["c", "d", "r", "z"], bubble.sort(array1)
 
   end
 
